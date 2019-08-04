7302650cec11a8f6ed11aad8767ec8ba357126a6
makiri <kireew.martin@icloud.com>
2019-08-04 21:45:17 +0200

    restructure mdx comps

--- src/components/mdx/PostComponents.tsx
+++ src/components/mdx/PostComponents.tsx
@@ -4,8 +4,6 @@ interface PostHeadingProps {
   children?: ReactNode
 }
 
-const PostHeading = ({ children }: PostHeadingProps): ReactElement => {
+export const PostHeading = ({ children }: PostHeadingProps): ReactElement => {
   return <h1 className="text-3xl">{children}</h1>
 }
-
-export default PostHeading
